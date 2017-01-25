00b7bf0ae1265702282c248ebf47769d6a015e57
Thecave3 <thecave003@hotmail.it>
2017-01-25 22:19:48 +0100

    various fixes

--- unix_client.c
+++ unix_client.c
@@ -2,13 +2,11 @@
 
 
 int main(int argc, char const *argv[]) {
-  int ret; //variabile sentinella per i valori
-  char* msg = malloc(sizeof(char)*ERR_MSG_LENGTH);
-  ret=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-  if (ret<0) ERROR_HELPER(ret,msg);
+  int ret;
 
+  ret= socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+  if (ret<0) ERROR_HELPER(ret,"Errore apertura socket");
+  printf("OK, ret is: %d\n",ret);
 
-
-    printf("OK, ret is: %d\n",ret);
   return 0;
 }
