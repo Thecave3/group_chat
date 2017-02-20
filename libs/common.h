78fc647c4d2f63fafc32dcc2c76add0b087b39d5
Giorgio Dramis <dramisgiorgio@gmail.com>
2017-02-20 13:24:25 +0100

    Update common.h

--- libs/common.h
+++ libs/common.h
@@ -1,1 +1,21 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
+#define ERR_MSG_LENGTH 1024
+
+#define GENERIC_ERROR_HELPER(cond, errCode, msg) do {               \
+        if (cond) {                                                 \
+            fprintf(stderr, "%s: %s\n", msg, strerror(errCode));    \
+            exit(EXIT_FAILURE);                                     \
+        }                                                           \
+    } while(0)
+
+#define ERROR_HELPER(ret, msg)          GENERIC_ERROR_HELPER((ret < 0), errno, msg)
+#define PTHREAD_ERROR_HELPER(ret, msg)  GENERIC_ERROR_HELPER((ret != 0), ret, msg)
