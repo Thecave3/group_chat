59f85c5ddc2a2f325232997b456beab239c78bd3
Giorgio Dramis <dramisgiorgio@gmail.com>
2017-02-28 13:59:14 +0100

    Update common.h

--- libs/common.h
+++ libs/common.h
@@ -1,21 +1,25 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <netdb.h>
 #include <stdio.h>
+#include <errno.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
-#include <ncurses.h>
 #include <unistd.h>
-#include <netinet/in.h>
+#include <ncurses.h>
+#include <pthread.h>
+#include <semaphore.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 
 #define ERR_MSG_LENGTH 1024
 #define SERVER_ADDRESS "127.0.0.1"
 #define SERVER_PORT 3000
+#define MAX_CONN_QUEUE 10
+#define DEBUG 1
 
 #define GENERIC_ERROR_HELPER(cond, errCode, msg) do {               \
         if (cond) {                                                 \
