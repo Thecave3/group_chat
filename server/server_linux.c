63a3e527cc88d76d7dc910e7172263afc39c413c
Giorgio Dramis <dramisgiorgio@outlook.com>
2017-03-01 11:07:51 +0100

    Update server_linux.c

--- server/server_linux.c
+++ server/server_linux.c
@@ -1,26 +1,11 @@
 #include "../libs/common.h"
+#include "../libs/server_protocol.h"
+#include "../libs/server_utils.h"
 
 #define WELCOME_MSG 	"THIS IS THE WELCOME MESSAGE OF SERVER\0"
 
-int nclients;
-
-typedef struct client_s {
-	int	client_id;
-	int	client_desc;
-	char	client_ip[16];
-	char	client_name[64];
-	struct client_s* next;
-} client_t;
-typedef client_t* client_l;
-
-client_l client_list;
-client_l last_client;
-sem_t client_list_semaphore;
-
-int recv_message(int socket_desc, char* buffer, int buffer_len);
-int send_message(int socket_desc, char* buffer, int buffer_len);
+int send_list(int socket_desc);
 void *init_client_routine(void* arg);
-void goodbye (void);
 
 int main(int argc, char const *argv[]) {
 	int									server_desc , client_desc, client_addr_len, ret;
@@ -30,20 +15,7 @@ int main(int argc, char const *argv[]) {
   signal(SIGINT, exit);
 
 	nclients = 0;
-	client_list = malloc(sizeof(client_t));
-	client_list->next = NULL;
 	client_addr_len = sizeof(client_addr);
-
-	// Controllo sui valori in input
-	if (argc != 2) {
-		fprintf(stderr, "usage: central_server: PORT\n");
-		exit(EXIT_FAILURE);
-	}
-	ret = atoi(argv[1]);
-	if(ret < 1024 && ret > 65535) {
-		fprintf(stderr, "Incorrect port value\n");
-		exit(EXIT_FAILURE);
-	}
 
 	if (sem_init(&client_list_semaphore, 0, 1)) {
 		if (DEBUG) {
@@ -61,33 +33,32 @@ int main(int argc, char const *argv[]) {
 	}
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
-	server_addr.sin_port = htons(ret);
+	server_addr.sin_port = htons(SERVER_PORT);
 	if(bind(server_desc,(struct sockaddr *)&server_addr , sizeof(server_addr)) < 0)
 	{
 		perror("Bind failed");
 		exit(EXIT_FAILURE);
 	}
 	listen(server_desc , MAX_CONN_QUEUE);
-
+	fprintf(stderr, "Server started!\n");
 	// Ciclo sentinella
 	while(1) {
-		fprintf(stderr, "Server started!\n");
 		client_desc = accept(server_desc, (struct sockaddr *)&client_addr, (socklen_t*)&client_addr_len);
     if (client_desc < 0) {
 			perror("Unable to connect to client");
 			return 1;
     }
-		fprintf(stderr, "Incoming connection");
+		fprintf(stderr, "Incoming connection by ");
 		client_l thread_arg = malloc(sizeof(client_t));
-		thread_arg->client_id = nclients;
 		thread_arg->client_desc = client_desc;
 		thread_arg->next = NULL;
 		sprintf(thread_arg->client_ip,
-			"%d.%d.%d.%d\0",
+			"%d.%d.%d.%d",
 			(int)(client_addr.sin_addr.s_addr&0xFF),
 			(int)((client_addr.sin_addr.s_addr&0xFF00)>>8),
 			(int)((client_addr.sin_addr.s_addr&0xFF0000)>>16),
 			(int)((client_addr.sin_addr.s_addr&0xFF000000)>>24));
+		fprintf(stderr, "%s\n", thread_arg->client_ip);
 		pthread_t* init_client_thread = malloc(sizeof(pthread_t));
 		ret = pthread_create(init_client_thread, NULL, init_client_routine,(void*) thread_arg);
 		if (ret != 0) {
@@ -102,87 +73,15 @@ int main(int argc, char const *argv[]) {
 			exit(EXIT_FAILURE);
 		}
 		memset(&client_addr, 0, sizeof(client_addr));
-		nclients++;
   }
   exit(EXIT_SUCCESS);
-}
-
-int recv_message(int socket_desc, char* buffer,  int buffer_len) {
-  int   ret;
-  int   bytes_read = 0;
-
-  while(1) {
-    ret = recv(socket_desc, buffer + bytes_read, 1, 0);
-    if (ret == -1 && errno == EINTR)
-			continue;
-    if (ret == -1) {
-			if (DEBUG)
-      	perror("recv_message: error in recv");
-      return -1;
-    }
-    if (ret == 0) {
-			if (DEBUG)
-				perror("recv_message: connection closed by client");
-      return 0;
-    }
-    bytes_read++;
-    if (buffer[bytes_read-1] == '\n' ||
-				buffer[bytes_read-1] ==  '\0' ||
-				bytes_read == buffer_len)
-			break;
-  }
-  buffer[bytes_read-1] = '\0';
-  return bytes_read;
-}
-
-int send_message(int socket_desc, char* buffer, int buffer_len) {
-  int   ret;
-  int   bytes_send = 0;
-
-  while (bytes_send < buffer_len) {
-    ret = send(socket_desc, buffer + bytes_send, buffer_len - bytes_send, 0);
-    if (ret == -1 && errno == EINTR) continue;
-    if (ret == -1) {
-			if (DEBUG)
-      	perror("send_message: error in send");
-      return -1;
-    }
-    bytes_send += ret;
-  }
-  return bytes_send;
-}
-
-int send_list(int socket_desc) {
-	client_l aux = client_list;
-	char buffer[84*nclients];
-	while (aux != NULL) {
-		strcpy(buffer, aux->client_name);
-		strcat(buffer, "\n");
-		strcat(buffer, aux->client_ip);
-		strcat(buffer, "\n\r");
-		aux = aux->next;
-	}
-	if (send_message(socket_desc, buffer, sizeof(buffer))){
-		if (DEBUG)
-			fprintf(stderr, "\tsend_list\n");
-		return 0;
-	}
-	return 1
 }
 
 void *init_client_routine(void *arg) {
 	int ret;
 	client_l client = (client_l) arg;
-	if (send_message(client->client_desc, WELCOME_MSG, sizeof(WELCOME_MSG))){
-		if (DEBUG)
-			fprintf(stderr, "\tinit_client_routine\n");
-		exit(EXIT_FAILURE);
-	}
-	if (recv_message(client->client_desc, client->client_name, sizeof(client->client_name))) {
-		if (DEBUG)
-			fprintf(stderr, "\tinit_client_routine\n");
-		exit(EXIT_FAILURE);
-	}
+	send_message(client->client_desc, WELCOME_MSG, sizeof(WELCOME_MSG));
+	recv_message(client->client_desc, client->client_name, sizeof(client->client_name));
 	// Aggiungo il client alla scl del client attivi
 	if (sem_wait(&client_list_semaphore)) {
 		if (DEBUG) {
@@ -191,14 +90,16 @@ void *init_client_routine(void *arg) {
 		}
 		exit(EXIT_FAILURE);
 	}
+	client->client_id = nclients;
 	if (nclients == 0) {
 		client_list = client;
-		last_client = client_list;
+		last_client = client;
 	}
 	else {
 		last_client->next = client;
-		last_client = last_client->next;
+		last_client = client;
 	}
+	nclients++;
 	if (sem_post(&client_list_semaphore)) {
 		if (DEBUG) {
 			perror("client_list_semaphore: error in post");
@@ -208,21 +109,11 @@ void *init_client_routine(void *arg) {
 	}
 	char buffer[128];
 	while(1) {
-		ret = recv_message(client->client_desc, buffer, sizeof(buffer)) {
+		ret = recv_message(client->client_desc, buffer, sizeof(buffer));
 		if (ret == 0) {
 			fprintf(stderr, "Connection closed by %s:%s\n", client->client_name, client->client_ip);
 			break;
 		}
 	}
 	pthread_exit(NULL);
-}
-
-void goodbye (void) {
-	while (client_list != NULL) {
-		client_l aux = client_list;
-		client_list = client_list->next;
-		free(aux);
-	}
-	sem_destroy(&client_list_semaphore);
-	fprintf(stderr, "\nThe server say goodbye!\n");
 }
