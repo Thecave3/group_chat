96f3914eda44fa10530b3789eed824db85544846
Giorgio Dramis <dramisgiorgio@gmail.com>
2017-04-15 01:23:57 +0000

    Apportate migliorie a porzioni di codice

--- server/server_header.c
+++ server/server_header.c
@@ -6,50 +6,44 @@ int 	server_routine(int argc, char const *argv[]) {
                       client_desc,                                               // Descrittore del client
                       client_addr_len,                                           // Lunghezza della struttura sockaddr_in del client
                       ret;                                                       // Lunghezza della struttura
-	struct sockaddr_in	server_addr ,                                              // Struttura sockaddr_in del server
+  struct sockaddr_in  server_addr ,                                              // Struttura sockaddr_in del server
                       client_addr;                                               // Struttura sockaddr_in del client
 
-	client_list = NULL;                                                            // Setto client_list a NULL
-	last_client = NULL;                                                            // Setto last_client a NULL
+  client_list = NULL;                                                            // Setto client_list a NULL
+  last_client = NULL;                                                            // Setto last_client a NULL
 
-	if (atexit(server_exit) != 0) {                                                // Collego all'esecuzione della funzione di terminazione del programma la funzione server_exit
-		exit(EXIT_FAILURE);                                                          // In caso di errore termino il programma
-	}
+  if (atexit(server_exit) != 0) {                                                // Collego all'esecuzione della funzione di terminazione del programma la funzione server_exit
+    exit(EXIT_FAILURE);                                                          // In caso di errore termino il programma
+  }
 
-	if (signal(SIGINT, exit) == SIG_ERR) {                                         // Collego al segnale SIGINT l'esecuzione della funzione di terminazione del programma
-		exit(EXIT_FAILURE);
-	}
+  if (signal(SIGINT, exit) == SIG_ERR) {                                         // Collego al segnale SIGINT l'esecuzione della funzione di terminazione del programma
+    exit(EXIT_FAILURE);
+  }
 
-	ret = server_init(&server_desc, &server_addr);                                 // Inizializzo il server tramite la funzione server_init()
-	if (ret < 1) exit(EXIT_FAILURE);                                               // Se ret è minore di uno si è verificato un errore e termino il programma
-	fprintf(stderr, "Server Started\n");                                           // Avverto l'utente che l'init del server si è concluso con successo
-	daemon(0,1);                                                                   // Metto il server in modalità demone
+  ret = server_init(&server_desc, &server_addr);                                 // Inizializzo il server tramite la funzione server_init()
+  if (ret < 1) exit(EXIT_FAILURE);                                               // Se ret è minore di uno si è verificato un errore e termino il programma
+  fprintf(stderr, "Server Started\n");                                           // Avverto l'utente che l'init del server si è concluso con successo
+  daemon(0,1);                                                                   // Metto il server in modalità demone
 
-	client_addr_len = sizeof(client_addr);                                         // Calcolo clinet_addr_len
-	while(1) {
-		memset(&client_addr, 0, sizeof(client_addr));                                // Resetto preventivamente la struttura client_addr                                                                     // Inizio del ciclo sentinella
-		client_desc = accept(server_desc,                                            // Attendo un eventuale connessione in ingresso
+  client_addr_len = sizeof(client_addr);                                         // Calcolo clinet_addr_len
+  while(1) {
+    memset(&client_addr, 0, sizeof(client_addr));                                // Resetto preventivamente la struttura client_addr                                                                     // Inizio del ciclo sentinella
+    client_desc = accept(server_desc,                                            // Attendo un eventuale connessione in ingresso
                          (struct sockaddr *)&client_addr,
                          (socklen_t*)&client_addr_len);
     if (client_desc < 0) continue;                                               // In caso di errore salto all'iterazione successiva
-		client_l thread_arg = malloc(sizeof(client_t));                              // Alloco la struttura per la memorizzazione delle informazioni riguardanti il client
-		thread_arg->client_desc = client_desc;                                       // Inserisco all'interno di tale struttura il descrittore del client
-		thread_arg->next = NULL;                                                     // Setto a NULL il campo next
-		thread_arg->prev = NULL;                                                     // Setto a NULL il campo prev
-		sprintf(thread_arg->client_ip,                                               // Mi ricavo l'ip del client
-			"%d.%d.%d.%d",
-			(int)(client_addr.sin_addr.s_addr&0xFF),
-			(int)((client_addr.sin_addr.s_addr&0xFF00)>>8),
-			(int)((client_addr.sin_addr.s_addr&0xFF0000)>>16),
-			(int)((client_addr.sin_addr.s_addr&0xFF000000)>>24));
-		pthread_t* init_client_thread = malloc(sizeof(pthread_t));                   // Alloco la memoria necessaria per la creazione del thread di gestione del client
-		ret = pthread_create(init_client_thread,                                     // Inizializzo il thread di gestione del client
+    client_l thread_arg = malloc(sizeof(client_t));                              // Alloco la struttura per la memorizzazione delle informazioni riguardanti il client
+    thread_arg->client_desc = client_desc;                                       // Inserisco all'interno di tale struttura il descrittore del client
+    thread_arg->next = NULL;                                                     // Setto a NULL il campo next
+    thread_arg->prev = NULL;                                                     // Setto a NULL il campo prev
+    pthread_t* init_client_thread = malloc(sizeof(pthread_t));                   // Alloco la memoria necessaria per la creazione del thread di gestione del client
+    ret = pthread_create(init_client_thread,                                     // Inizializzo il thread di gestione del client
                          NULL,
                          client_routine,
                          (void*) thread_arg);
-		if (ret != 0) continue;                                                      // In caso di errore passo all'iterazione successiva
-		ret = pthread_detach(*init_client_thread);                                   // Marchio il thread come detached
-		if (ret != 0) continue;                                                      // In caso di errore passo all'iterazione successiva
+    if (ret != 0) continue;                                                      // In caso di errore passo all'iterazione successiva
+    ret = pthread_detach(*init_client_thread);                                   // Marchio il thread come detached
+    if (ret != 0) continue;                                                      // In caso di errore passo all'iterazione successiva
   }
   exit(EXIT_SUCCESS);                                                            // Termino il server
 }
@@ -106,37 +100,44 @@ void*	client_routine(void *arg) {
 		if (strcmp(query, "STOF\0") == 0 && *status != OFFLINE) *status = OFFLINE;
 		if (strcmp(query, "STON\0") == 0 && *status != ONLINE) *status = ONLINE;
 		if (strcmp(query, "LIST\0") == 0) send_cl(client_desc);
+		if (strcmp(query, "CONN\0") == 0) {
+
+    }
 	}
 	pthread_exit(NULL);
 }
 
-int		server_init(int* sock_desc, struct sockaddr_in* sock_addr) {
-	nclients = 0;
-	client_list = NULL;
-	last_client = NULL;
+/* Routine di inizializzazione del server */
+int    server_init(int* sock_desc, struct sockaddr_in* sock_addr) {
+  nclients = 0;                                                                   // Resetto nclients
+  client_list = NULL;                                                             // Resetto client_list
+  last_client = NULL;                                                             // Resetto last_client
 
-	if (sem_init(&client_list_semaphore, 0, 1)) return 0;
+  if (sem_init(&client_list_semaphore, 0, 1)) return 0;                           // Creo il semaforo per la gestione della lista dei client
 
-	// Inizializzazione porta socket
-	*sock_desc = socket(AF_INET , SOCK_STREAM , 0);
-	if (*sock_desc == -1) exit(EXIT_FAILURE);
-	sock_addr->sin_family = AF_INET;
-	sock_addr->sin_addr.s_addr = INADDR_ANY;
-	sock_addr->sin_port = htons(SERVER_PORT);
-	if(bind(*sock_desc,(struct sockaddr *)sock_addr , sizeof(*sock_addr)) < 0) return -1;
-	if(listen(*sock_desc , MAX_CONN_QUEUE)) return -1;
-	return 1;
+  *sock_desc = socket(AF_INET , SOCK_STREAM , 0);                                 // Inizializzo la porta socket per la connessione
+  if (*sock_desc == -1) exit(EXIT_FAILURE);                                       // In caso di errore termino il server
+  sock_addr->sin_family = AF_INET;                                                // Popolo la struttura sock_addr
+  sock_addr->sin_addr.s_addr = INADDR_ANY;
+  sock_addr->sin_port = htons(SERVER_PORT);
+  if(bind(*sock_desc,                                                             // Effettuo la bind della socket e ritorno -1 in caso di errore
+          (struct sockaddr *)sock_addr ,
+          sizeof(*sock_addr)) < 0)
+    return -1;
+  if(listen(*sock_desc , MAX_CONN_QUEUE)) return -1;                              // Pongo la socket in modalità listen e ritorno -1 in caso di errore
+  return 1;                                                                       // Se tutto è andato a buon fine ritorno 1
 }
 
-void 	server_exit () {
-	sem_wait(&client_list_semaphore);
-	client_l aux;
-	while (client_list != NULL) {
-		aux = client_list;
-		client_list = client_list->next;
-		free(aux);
-	}
-	sem_post(&client_list_semaphore);
-	sem_destroy(&client_list_semaphore);
-	fprintf(stderr, "Server Halted\n");
+/* Funzione di terminazione del server */
+void    server_exit () {
+  sem_wait(&client_list_semaphore);                                              // Blocca il semaforo per la gestione della lista dei client
+  client_l aux;                                                                  // Dichiaro una variabile di tipo client_l
+  while (client_list != NULL) {                                                  // Fintanto che client_list è diverso da NULL quindi se c'é almeno un client connesso
+    aux = client_list;                                                           // Faccio puntare aux alla stessa zona di memoria di client_list
+    client_list = client_list->next;                                             // Sposto client_list sull'elemto successivo
+    free(aux);                                                                   // Libero la zona di memoria puntata da aux
+  }
+  sem_post(&client_list_semaphore);                                              // Sblocco il semaforo per la gestione della lista dei client
+  sem_destroy(&client_list_semaphore);                                           // Distruggo il semaforo per la gestione della lista dei client
+  fprintf(stderr, "Server Halted\n");                                            // Stampo un messaggio per avvertire l'utente che il server è terminato
 }
