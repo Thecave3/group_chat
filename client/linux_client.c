fd3d450ea8e4b8d0c7c947975f3778a25ed4022e
Andrea Lacava <thecave003@hotmail.it>
2017-06-29 19:10:36 +0200

    Aggiunta gestione di quit doppio in caso di chat
    DA TESTARE

--- client/linux_client.c
+++ client/linux_client.c
@@ -13,16 +13,19 @@ void kill_handler() {
   size_t close_command_len = strlen(close_command);
   int bytes_written = 0;
   int ret;
-  while (1) {
-    ret = write(socket_desc,close_command+bytes_written,close_command_len);
-    if (ret==-1) {
-      if (errno == EINTR) {
-        fprintf(stderr,"Errore scrittura dati, ripeto\n");
-        continue;
-      }
+  int end = 1 + (int) onChat;
+  for(int i = 0; i < end; i++){
+    while (1) {
+      ret = write(socket_desc,close_command+bytes_written,close_command_len);
+      if (ret==-1) {
+        if (errno == EINTR) {
+          fprintf(stderr,"Errore scrittura dati, ripeto\n");
+          continue;
+        }
       ERROR_HELPER(ret,"Errore scrittura dati fatale, panico");
     } else if ((bytes_written += ret) == close_command_len) break;
   }
+}
   printf("Chiusura connessione effettuata, bye bye\n");
   exit(EXIT_SUCCESS);
 }
