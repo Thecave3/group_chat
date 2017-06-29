ba84fd231f595c263f55979189dc0f954e43ac06
Thecave3 <thecave003@hotmail.it>
2017-06-30 00:11:24 +0200

    another fucking fix

--- client/linux_client.c
+++ client/linux_client.c
@@ -13,18 +13,6 @@ void kill_handler() {
   size_t close_command_len = strlen(close_command);
   int bytes_written = 0;
   int ret;
-<<<<<<< HEAD
-  int end = 1 + (int) onChat;
-  int i;
-  for(i = 0; i < end; i++){
-    while (1) {
-      ret = write(socket_desc,close_command+bytes_written,close_command_len);
-      if (ret==-1) {
-        if (errno == EINTR) {
-          fprintf(stderr,"Errore scrittura dati, ripeto\n");
-          continue;
-        }
-=======
   while (1) {
     ret = write(socket_desc,close_command+bytes_written,close_command_len);
     if (ret==-1) {
@@ -32,7 +20,6 @@ void kill_handler() {
         fprintf(stderr,"Errore scrittura dati, ripeto\n");
         continue;
       }
->>>>>>> dcbecb6a5c73b41bd1970702ea489dc5abd777f0
       ERROR_HELPER(ret,"Errore scrittura dati fatale, panico");
     } else if ((bytes_written += ret) == close_command_len) break;
   }
